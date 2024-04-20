76af1e0940cf43de6d49f62b401744854c20adc6
derpygamer2142 <derpygamer2142@gmail.com>
2024-04-20 15:50:42 -0700

    logging in?
    I don't know how to get the error from a login

--- src/routes/setup/+page.svelte
+++ src/routes/setup/+page.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
     import { onMount } from "svelte";
-
-	
+	import * as mjs from "@meower-media/meower"
+	const client = new mjs.Client()
 
 	// Upon login set the user and stuff and go to home
     let isClient = false;
+	let username = ""
+	let pswd = ""
+	let loginStatus = null
 
 	onMount(() => {
         isClient = true;
     })
+	console.log(client.user)
+
+	client.onLogin(() => {
+		loginStatus = "Logged in!"
+		console.log(client.user !== null && typeof client.user !== "undefined")
+		console.log("yay logged in wow so cool")
+	})
+
+	async function submit() {
+		loginStatus = "Logging in..."
+		client.login(username,pswd)
+		
+	}
 </script>
 
 <svelte:head>
@@ -20,10 +36,13 @@
 	<div class='login'>
 		<h1>Meower</h1>
 		<form>
-			<input type='text' placeholder='Username' class='login-input text'>
-			<input type='password' placeholder='Password' class='login-input text'>
-			<button class='login-input text'>Log in</button>
+			<input type='text' placeholder='Username' class='login-input text' bind:value={username}>
+			<input type='password' placeholder='Password' class='login-input text' bind:value={pswd}>
+			<button class='login-input text' on:click={submit}>Log in</button>
 		</form>
+		{#if loginStatus !== null}
+			<p>{loginStatus}</p>
+		{/if}
 		<small>Meower Example client.</small> 
 		
 	</div>
