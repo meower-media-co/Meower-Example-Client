18b98438622038cfffc9c1c21018e96ee2f84a5b
derpygamer2142 <93667155+derpygamer2142@users.noreply.github.com>
2024-04-30 11:27:14 -0700

    send button

--- src/lib/MessageInput.svelte
+++ src/lib/MessageInput.svelte
@@ -6,15 +6,25 @@
 </script>
 
 <!--should this be a textarea?-->
-
-{#if loggedin}
-    <input placeholder="Meow meow?"/>
-{:else}
-    <input placeholder="You need to login to send messages." disabled/>
-{/if}
-
+<div class="message">
+    {#if loggedin}
+        <input placeholder="Meow meow?"/>
+    {:else}
+        <input placeholder="You need to login to send messages." disabled/>
+    {/if}
+    <button class="send">idk</button>
+</div>
 <style>
+    .message {
+        display: inline-flex;
+    }
     input {
-        width: 700px;
+        width: 675px;
+        height: 25px;
+    }
+    .send {
+        position: relative;
+        width: 75px;
+        height: 50px;
     }
 </style>
